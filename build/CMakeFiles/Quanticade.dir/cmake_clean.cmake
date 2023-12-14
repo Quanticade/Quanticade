fa0e3fcc8769a04d242e04b079a71577b28e3c3c
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2023-12-14 18:31:04 +0100

    whole bunch of changes

---
+++ build/CMakeFiles/Quanticade.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Quanticade.dir/src/quanticade.c.o"
+  "CMakeFiles/Quanticade.dir/src/quanticade.c.o.d"
+  "Quanticade"
+  "Quanticade.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Quanticade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Quanticade.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Quanticade.
+# This may be replaced when dependencies are built.
