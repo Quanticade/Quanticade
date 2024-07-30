a4c1c4fbe53b652cb86cdbfb99d2304b6e194066
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2024-07-30 17:35:10 +0200

    uci.c: Clear history moves on ucinewgame
    bench: 2176547

--- Source/uci.c
+++ Source/uci.c
@@ -572,6 +572,9 @@ void uci_loop(position_t *pos, thread_t *threads, int argc, char *argv[]) {
     else if (strncmp(input, "ucinewgame", 10) == 0) {
       // clear hash table
       clear_hash_table();
+      for (int i = 0; i < thread_count; ++i) {
+        memset(threads[i].history_moves, 0, sizeof(threads[i].history_moves));
+      }
     }
     // parse UCI "go" command
     else if (strncmp(input, "go", 2) == 0) {
@@ -608,7 +611,7 @@ void uci_loop(position_t *pos, thread_t *threads, int argc, char *argv[]) {
       printf("option name Clear Hash type button\n");
       printf("option name LMP_BASE type spin default 6 min 1 max 12\n");
       printf("option name LMP_MULTIPLIER type spin default 2 min 1 max 4\n");
-      //printf("option name SyzygyPath type string default <empty>\n");
+      // printf("option name SyzygyPath type string default <empty>\n");
       printf("uciok\n");
     }
 
@@ -662,11 +665,9 @@ void uci_loop(position_t *pos, thread_t *threads, int argc, char *argv[]) {
       char *ptr = input + 32;
       // tb_init(ptr);
       printf("info string set SyzygyPath to %s\n", ptr);
-    }
-    else if (!strncmp(input, "setoption name LMP_BASE value ", 30)) {
+    } else if (!strncmp(input, "setoption name LMP_BASE value ", 30)) {
       sscanf(input, "%*s %*s %*s %*s %d", &LMP_BASE);
-    }
-    else if (!strncmp(input, "setoption name LMP_MULTIPLIER value ", 36)) {
+    } else if (!strncmp(input, "setoption name LMP_MULTIPLIER value ", 36)) {
       sscanf(input, "%*s %*s %*s %*s %d", &LMP_BASE);
     }
   }
