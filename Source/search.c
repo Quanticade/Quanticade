2a61ef6b930c3cd4c10cea6ac4a360720a11aa2c
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2024-10-09 18:33:27 +0200

    search.c: Use mate_score in FP check (#67)
    bench: 8748849

--- Source/search.c
+++ Source/search.c
@@ -827,7 +827,7 @@ static inline int negamax(position_t *pos, thread_t *thread, searchstack_t *ss,
     int lmr_depth = MAX(1, depth - 1 - MAX(r, 1));
 
     // Futility Pruning
-    if (!root_node && score > -mate_value && lmr_depth <= 5 && !in_check &&
+    if (!root_node && score > -mate_score && lmr_depth <= 5 && !in_check &&
         quiet && ss->static_eval + lmr_depth * 150 + 150 <= alpha) {
       continue;
     }
