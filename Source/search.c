3f5889d218e8a537311221287c8716790f87467e
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2024-10-19 09:48:50 +0200

    search.c: Fix LMR improving
    bench: 3487386

--- Source/search.c
+++ Source/search.c
@@ -937,7 +937,9 @@ static inline int negamax(position_t *pos, thread_t *thread, searchstack_t *ss,
     R -= (quiet ? history_score / HISTORY_MAX : 0);
     R -= in_check;
     R += cutnode;
-    R += improving;
+    if (!improving) {
+      R++;
+    }
 
     if (depth > 1 && legal_moves > 1) {
       R = clamp(R, 1, new_depth);
