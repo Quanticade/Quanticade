ea8cc32e87daa73a8d844106c46b56f3557dad76
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2024-11-21 15:39:09 +0100

    evaluate.c: Limit 50 move rule to 0.5 as minimum (#96)
    bench: 4223374

--- Source/evaluate.c
+++ Source/evaluate.c
@@ -3,6 +3,7 @@
 #include "enums.h"
 #include "nnue.h"
 #include "structs.h"
+#include "uci.h"
 
 nnue_t nnue_data;
 extern nnue_settings_t nnue_settings;
@@ -17,5 +18,6 @@ int evaluate(position_t *pos, accumulator_t *accumulator) {
 
   eval = eval * (200 + phase) / 256;
   float fifty_move_scaler = (float)((100 - (float)pos->fifty) / 100);
+  fifty_move_scaler = MAX(fifty_move_scaler, 0.5f);
   return (int)(eval * fifty_move_scaler);
 }
