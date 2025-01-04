d3ca8ce4e4666f00e4c7579b93f338d636ed8fee
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-01-04 22:49:17 +0100

    evaluate.c: Scale mat with SEE piece values
    bench: 3968119

--- Source/evaluate.c
+++ Source/evaluate.c
@@ -8,15 +8,17 @@
 nnue_t nnue_data;
 extern nnue_settings_t nnue_settings;
 
+extern int SEEPieceValues[];
+
 int evaluate(position_t *pos, accumulator_t *accumulator) {
   int eval = nnue_evaluate(pos, accumulator);
 
-  int phase = 3 * popcount(pos->bitboards[n] | pos->bitboards[N]) +
-              3 * popcount(pos->bitboards[b] | pos->bitboards[B]) +
-              5 * popcount(pos->bitboards[r] | pos->bitboards[R]) +
-              10 * popcount(pos->bitboards[q] | pos->bitboards[Q]);
+  int phase = SEEPieceValues[KNIGHT] * popcount(pos->bitboards[n] | pos->bitboards[N]) +
+              SEEPieceValues[BISHOP] * popcount(pos->bitboards[b] | pos->bitboards[B]) +
+              SEEPieceValues[ROOK] * popcount(pos->bitboards[r] | pos->bitboards[R]) +
+              SEEPieceValues[QUEEN] * popcount(pos->bitboards[q] | pos->bitboards[Q]);
 
-  eval = eval * (200 + phase) / 256;
+  eval = eval * (26500 + phase) / 32768;
   float fifty_move_scaler = (float)((100 - (float)pos->fifty) / 100);
   fifty_move_scaler = MAX(fifty_move_scaler, 0.5f);
   return (int)(eval * fifty_move_scaler);
