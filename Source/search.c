c301db8179c83aa81560df0ebdc5f6c859dc97f2
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-02-10 17:30:07 +0100

    search.c: Double/Triple extensions depth increase
    bench: 10404007

--- Source/search.c
+++ Source/search.c
@@ -701,6 +701,7 @@ static inline int negamax(position_t *pos, thread_t *thread, searchstack_t *ss,
         if (!get_move_capture(move) && s_score + SE_TRIPLE_MARGIN < s_beta) {
           extensions++;
         }
+        depth += depth < 10;
       }
 
       // Multicut: Singular search failed high so if singular beta beats our
