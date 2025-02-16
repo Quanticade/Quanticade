890531d3da3e05ce7426c9f0b6455ed465859182
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2025-02-16 10:04:09 +0100

    search.c: DE in pv_node with margin (#133)
    bench: 10642286

--- Source/search.c
+++ Source/search.c
@@ -708,7 +708,7 @@ static inline int negamax(position_t *pos, thread_t *thread, searchstack_t *ss,
       // No move beat tt score so we extend the search
       if (s_score < s_beta) {
         extensions++;
-        if (!pv_node) {
+        if (s_score < s_beta - 2 * pv_node) {
           extensions++;
         }
         if (!get_move_capture(move) && s_score + SE_TRIPLE_MARGIN < s_beta) {
