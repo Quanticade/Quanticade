4cc5e975a1be95ff919a9a97110de3b830f36570
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-02-19 15:31:05 +0100

    search.c: Reduce more based on move count
    bench: 8455177

--- Source/search.c
+++ Source/search.c
@@ -795,6 +795,7 @@ static inline int negamax(position_t *pos, thread_t *thread, searchstack_t *ss,
       R -= in_check * LMR_IN_CHECK;
       R += cutnode * LMR_CUTNODE;
       R -= (tt_depth >= depth) * LMR_TT_DEPTH;
+      R += legal_moves * 64;
       R = clamp(R / 1024, 1, new_depth);
       current_score = -negamax(pos, thread, ss + 1, -alpha - 1, -alpha,
                                new_depth - R + 1, 1);
