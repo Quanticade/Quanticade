b63d91bb4339ab853aca7bf20287d208e7602479
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-02-23 16:55:05 +0100

    search.c: Make it more clear LMR happens from depth 2 included
    bench: 11609458

--- Source/search.c
+++ Source/search.c
@@ -797,7 +797,7 @@ static inline int negamax(position_t *pos, thread_t *thread, searchstack_t *ss,
     // PVS & LMR
     int new_depth = depth + extensions - 1;
 
-    if (depth > 1 && moves_seen > 2 + 2 * pv_node) {
+    if (depth >= 2 && moves_seen > 2 + 2 * pv_node) {
       int R = lmr[quiet][depth][MIN(255, moves_seen)] * 1024;
       R += !pv_node * LMR_PV_NODE;
       R -= ss->history_score * (quiet ? LMR_HISTORY_QUIET : LMR_HISTORY_NOISY) /
