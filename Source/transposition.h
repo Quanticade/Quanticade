df05e8e457025afc2bca3c2f0a4b039054a2c4ce
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-02-26 11:37:22 +0100

    Source: Refactor and fix TT score usage
    bench: 13335988

--- Source/transposition.h
+++ Source/transposition.h
@@ -19,8 +19,8 @@ extern tt_t tt;
 
 void clear_hash_table(void);
 void prefetch_hash_entry(uint64_t hash_key);
-uint8_t read_hash_entry(position_t *pos, uint16_t *move, int16_t *tt_score,
-                        uint8_t *tt_depth, uint8_t *tt_flag, uint8_t *tt_pv);
+uint8_t can_use_score(int alpha, int beta, int tt_score, uint8_t flag);
+uint8_t read_hash_entry(position_t *pos, tt_entry_t *tt_entry);
 void write_hash_entry(position_t *pos, int16_t score, uint8_t depth,
                       uint16_t move, uint8_t hash_flag, uint8_t tt_pv);
 void init_hash_table(uint64_t mb);
