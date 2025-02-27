2e6eca5d4e6c19242ced8b509baa5e26e7352b8d
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2025-02-27 08:44:47 +0100

    search.c: Do not extend if our ply is > 2*rootdepth (#142)
    This helps the search not get stuck in some positions
    
    bench: 13636669

--- Source/search.c
+++ Source/search.c
@@ -703,7 +703,7 @@ static inline int negamax(position_t *pos, thread_t *thread, searchstack_t *ss,
     // A rather simple idea that if our TT move is accurate we run a reduced
     // search to see if we can beat this score. If not we extend the TT move
     // search
-    if (!root_node && depth >= SE_DEPTH && move == tt_move &&
+    if (pos->ply < thread->depth * 2 && !root_node && depth >= SE_DEPTH && move == tt_move &&
         !ss->excluded_move && tt_depth >= depth - SE_DEPTH_REDUCTION &&
         tt_flag != HASH_FLAG_UPPER_BOUND && abs(tt_score) < MATE_SCORE) {
       const int s_beta = tt_score - depth;
