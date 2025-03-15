e109e2b1d3978f0f88d660eb102917346291a318
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-03-15 20:31:59 +0100

    search.c: Update WDL normalization value
    bench: 4891253

--- Source/search.c
+++ Source/search.c
@@ -974,7 +974,7 @@ static void print_thinking(thread_t *thread, int score, int current_depth) {
   } else if (score > MATE_SCORE && score < MATE_VALUE) {
     printf("mate %d ", (MATE_VALUE - score) / 2 + 1);
   } else {
-    printf("cp %d ", 100 * score / 212);
+    printf("cp %d ", 100 * score / 215);
   }
   printf("nodes %" PRIu64 " ", nodes);
   printf("nps %" PRIu64 " ", nps);
