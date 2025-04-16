93339a0529a966802e56ff179d5c0516c3ffc041
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2025-04-16 07:22:32 +0200

    pawnhist (#176)
    bench: 2898741

--- Source/history.c
+++ Source/history.c
@@ -66,7 +66,8 @@ uint64_t generate_pawn_key(position_t *pos) {
 
 int16_t calculate_corrhist_bonus(int16_t static_eval, int16_t search_score,
                                  uint8_t depth) {
-  return clamp((search_score - static_eval) * depth / 8, -CORR_HISTORY_MINMAX, CORR_HISTORY_MINMAX);
+  return clamp((search_score - static_eval) * depth / 8, -CORR_HISTORY_MINMAX,
+               CORR_HISTORY_MINMAX);
 }
 
 int16_t scale_corrhist_bonus(int16_t score, int16_t bonus) {
@@ -84,9 +85,10 @@ uint8_t static_eval_within_bounds(int16_t static_eval, int16_t score,
 int16_t adjust_static_eval(thread_t *thread, position_t *pos,
                            int16_t static_eval) {
   const int pawn_correction =
-      thread->correction_history[pos->side][pos->hash_keys.pawn_key & 16383] * PAWN_CORR_HISTORY_MULTIPLIER;
+      thread->correction_history[pos->side][pos->hash_keys.pawn_key & 16383] *
+      PAWN_CORR_HISTORY_MULTIPLIER;
   const int adjusted_score = static_eval + pawn_correction / 1024;
-  //printf("%d %d %d\n", pawn_correction / 1024, adjusted_score, static_eval);
+  // printf("%d %d %d\n", pawn_correction / 1024, adjusted_score, static_eval);
   return clamp(adjusted_score, -MATE_SCORE + 1, MATE_SCORE - 1);
 }
 
@@ -96,7 +98,8 @@ void update_pawn_corrhist(thread_t *thread, int16_t static_eval, int16_t score,
     return;
   }
   int16_t bonus = calculate_corrhist_bonus(static_eval, score, depth);
-  thread->correction_history[thread->pos.side][thread->pos.hash_keys.pawn_key & 16383] +=
+  thread->correction_history[thread->pos.side]
+                            [thread->pos.hash_keys.pawn_key & 16383] +=
       scale_corrhist_bonus(
           thread->correction_history[thread->pos.side]
                                     [thread->pos.hash_keys.pawn_key & 16383],
@@ -157,6 +160,23 @@ static inline void update_continuation_history(thread_t *thread,
           abs(adjust) / HISTORY_MAX;
 }
 
+static inline void update_pawn_history(thread_t *thread, int move,
+                                       uint8_t depth, uint8_t is_best_move) {
+  int target = get_move_target(move);
+  int source = get_move_source(move);
+  int bonus = 16 * depth * depth + 32 * depth + 16;
+  int clamped_bonus =
+      clamp(bonus, -QUIET_HISTORY_BONUS_MIN, QUIET_HISTORY_BONUS_MAX);
+  int clamped_malus =
+      clamp(bonus, -QUIET_HISTORY_MALUS_MIN, QUIET_HISTORY_MALUS_MAX);
+  int adjust = is_best_move ? clamped_bonus : -clamped_malus;
+  thread->pawn_history[thread->pos.hash_keys.pawn_key % 32767]
+                      [thread->pos.mailbox[source]][target] +=
+      adjust - thread->pawn_history[thread->pos.hash_keys.pawn_key % 32767]
+                                   [thread->pos.mailbox[source]][target] *
+                   abs(adjust) / HISTORY_MAX;
+}
+
 void update_quiet_history_moves(thread_t *thread, moves *quiet_moves,
                                 int best_move, uint8_t depth) {
   for (uint32_t i = 0; i < quiet_moves->count; ++i) {
@@ -194,6 +214,17 @@ void update_continuation_history_moves(thread_t *thread, searchstack_t *ss,
                                   depth, 0);
       update_continuation_history(thread, ss - 4, quiet_moves->entry[i].move,
                                   depth, 0);
+    }
+  }
+}
+
+void update_pawn_history_moves(thread_t *thread, moves *quiet_moves,
+                               int best_move, uint8_t depth) {
+  for (uint32_t i = 0; i < quiet_moves->count; ++i) {
+    if (quiet_moves->entry[i].move == best_move) {
+      update_pawn_history(thread, best_move, depth, 1);
+    } else {
+      update_pawn_history(thread, quiet_moves->entry[i].move, depth, 0);
     }
   }
 }
