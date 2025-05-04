e78fa544de568f9d2ab31dc4b95e7942d464fd4a
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-05-04 16:25:48 +0200

    search.c: More reductions if node was in PV but isnt anymore
    bench: 2239145

--- Source/search.c
+++ Source/search.c
@@ -882,7 +882,7 @@ static inline int16_t negamax(position_t *pos, thread_t *thread,
     int new_depth = depth + extensions - 1;
 
     int R = lmr[quiet][depth][MIN(255, moves_seen)] * 1024;
-    R += !pv_node * LMR_PV_NODE;
+    R += (!pv_node + ss->tt_pv) * LMR_PV_NODE;
     R -= ss->history_score * (quiet ? LMR_HISTORY_QUIET : LMR_HISTORY_NOISY) /
          (quiet ? LMR_QUIET_HIST_DIV : LMR_CAPT_HIST_DIV);
     R -= in_check * LMR_IN_CHECK;
