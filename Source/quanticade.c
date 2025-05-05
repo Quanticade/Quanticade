c721fed6daac386f9058b90a76cadcb98bfd1eb3
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2025-05-05 20:14:10 +0200

    Garmr net (#186)
    bench: 2877394

--- Source/quanticade.c
+++ Source/quanticade.c
@@ -116,7 +116,7 @@ void init_all(void) {
   // init hash table with default size
   init_hash_table(default_hash_size);
 
-  nnue_init("freki.nnue");
+  nnue_init("garmr.nnue");
 }
 
 /**********************************\
@@ -136,7 +136,7 @@ int main(int argc, char *argv[]) {
   tt.hash_entry = NULL;
   tt.num_of_entries = 0;
   nnue_settings.nnue_file = calloc(21, 1);
-  strcpy(nnue_settings.nnue_file, "freki.nnue");
+  strcpy(nnue_settings.nnue_file, "garmr.nnue");
   // init all
   init_all();
 
