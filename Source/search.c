40bc3c3843eb59b923a2e33df0e082c540919c61
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-06-23 17:39:56 +0200

    search.c: Use eval in FP
    bench: 2416308

--- Source/search.c
+++ Source/search.c
@@ -697,8 +697,8 @@ static inline int16_t negamax(position_t *pos, thread_t *thread,
 
       /* search moves with reduced depth to find beta cutoffs
          depth - 1 - R where R is a reduction limit */
-      current_score = -negamax(&pos_copy, thread, ss + 1, -beta, -beta + 1, depth - R,
-                               !cutnode, NON_PV);
+      current_score = -negamax(&pos_copy, thread, ss + 1, -beta, -beta + 1,
+                               depth - R, !cutnode, NON_PV);
 
       (ss + 1)->null_move = 0;
 
@@ -795,7 +795,7 @@ static inline int16_t negamax(position_t *pos, thread_t *thread,
     // Futility Pruning
     if (!root_node && current_score > -MATE_SCORE && lmr_depth <= FP_DEPTH &&
         !in_check && quiet &&
-        ss->static_eval + lmr_depth * FP_MULTIPLIER + FP_ADDITION <= alpha) {
+        ss->eval + lmr_depth * FP_MULTIPLIER + FP_ADDITION <= alpha) {
       skip_quiets = 1;
       continue;
     }
