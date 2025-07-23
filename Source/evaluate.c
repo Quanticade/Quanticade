51d620d4ef54d154b45b5c3c5f3566d8ae4e364d
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-07-23 11:10:48 +0200

    evaluate.c: Material count
    bench: 2653653

--- Source/evaluate.c
+++ Source/evaluate.c
@@ -8,22 +8,44 @@
 nnue_t nnue_data;
 extern nnue_settings_t nnue_settings;
 
-int EVAL_KNIGHT = 384;
-int EVAL_BISHOP = 384;
-int EVAL_ROOK = 640;
-int EVAL_QUEEN = 1280;
-int EVAL_SCALE_BASE = 25600;
+int16_t evaluate(thread_t *thread, position_t *pos, accumulator_t *accumulator) {
+  (void)thread;
+  (void)accumulator;
+  int eval = 0;
+  for (uint8_t sq = 0; sq < 64; sq++) {
+    if (pos->side == 0) {
+      switch (pos->mailbox[sq]) {
+        case P: eval += 100; break;
+        case p: eval -= 100; break;
+        case N: eval += 300; break;
+        case n: eval -= 300; break;
+        case B: eval += 330; break;
+        case b: eval -= 330; break;
+        case R: eval += 500; break;
+        case r: eval -= 500; break;
+        case Q: eval += 900; break;
+        case q: eval -= 900; break;
+        default: break;
+      }
+    }
+    else {
+      switch (pos->mailbox[sq]) {
+        case P: eval -= 100; break;
+        case p: eval += 100; break;
+        case N: eval -= 300; break;
+        case n: eval += 300; break;
+        case B: eval -= 330; break;
+        case b: eval += 330; break;
+        case R: eval -= 500; break;
+        case r: eval += 500; break;
+        case Q: eval -= 900; break;
+        case q: eval += 900; break;
+        default: break;
+      }
+    }
+  }
 
-int16_t evaluate(thread_t *thread, position_t *pos,
-                 accumulator_t *accumulator) {
-  int eval = nnue_evaluate(thread, pos, accumulator);
 
-  int phase = EVAL_KNIGHT * popcount(pos->bitboards[n] | pos->bitboards[N]) +
-              EVAL_BISHOP * popcount(pos->bitboards[b] | pos->bitboards[B]) +
-              EVAL_ROOK * popcount(pos->bitboards[r] | pos->bitboards[R]) +
-              EVAL_QUEEN * popcount(pos->bitboards[q] | pos->bitboards[Q]);
-
-  eval = eval * (EVAL_SCALE_BASE + phase) / 32768;
-  eval = clamp(eval, -MATE_SCORE + 1, MATE_SCORE - 1);
-  return (int16_t)eval;
+  int16_t final_eval = clamp(eval, -MATE_SCORE + 1, MATE_SCORE - 1);
+  return final_eval;
 }
