88ab1c02b4c3a88ff7c068937f1fbb45e948d23c
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-07-25 10:57:18 +0200

    search.c: Reduce less if move gives a check
    bench: 2225353

--- Source/search.c
+++ Source/search.c
@@ -918,6 +918,7 @@ static inline int16_t negamax(position_t *pos, thread_t *thread,
 
     int R = lmr[quiet][depth][MIN(255, moves_seen)] * 1024;
     R += !pv_node * LMR_PV_NODE;
+    R -= might_give_check(pos, move) * 1024;
     R -= ss->history_score * (quiet ? LMR_HISTORY_QUIET : LMR_HISTORY_NOISY) /
          (quiet ? LMR_QUIET_HIST_DIV : LMR_CAPT_HIST_DIV);
     R -= in_check * LMR_IN_CHECK;
