37c18274c01db6bce9023c5ef4e86bd84c22eefc
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2025-07-27 11:13:13 +0200

    search.c: Disable RFP if we are in ttpv node (#226)
    bench: 2273315

--- Source/search.c
+++ Source/search.c
@@ -651,7 +651,7 @@ static inline int16_t negamax(position_t *pos, thread_t *thread,
       ++depth;
     }
     // Reverse Futility Pruning
-    if (depth <= RFP_DEPTH &&
+    if (!ss->tt_pv && depth <= RFP_DEPTH &&
         ss->eval >= beta + RFP_BASE_MARGIN + RFP_MARGIN * depth -
                         RFP_IMPROVING * improving -
                         RFP_OPP_WORSENING * opponent_worsening) {
