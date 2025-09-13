6d8e5010ba6cdf6b959f6c7dd2e4743114bf302b
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-09-13 08:52:46 +0200

    search.c: Allow extensions in PV nodes
    bench: 3204076

--- Source/search.c
+++ Source/search.c
@@ -1020,7 +1020,7 @@ static inline int16_t negamax(position_t *pos, thread_t *thread,
       ss->reduction = R;
 
       R = R / 1024;
-      int reduced_depth = MAX(1, MIN(new_depth - R, new_depth)) + pv_node;
+      int reduced_depth = MAX(1, MIN(new_depth - R, new_depth + pv_node)) + pv_node;
 
       current_score = -negamax(pos, thread, ss + 1, -alpha - 1, -alpha,
                                reduced_depth, 1, NON_PV);
