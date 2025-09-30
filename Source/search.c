4ad5a24ab86a831fb820cb34a668c7d2af9c13ff
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-09-30 20:45:40 +0200

    search.c: Check for TT stuff in probcut
    bench: 3288313

--- Source/search.c
+++ Source/search.c
@@ -830,8 +830,9 @@ static inline int16_t negamax(position_t *pos, thread_t *thread,
   }
 
   // ProbCut pruning
-  if (!pv_node && !in_check && !ss->excluded_move && depth >= PROBCUT_DEPTH &&
-      abs(beta) < MATE_SCORE) {
+  if (!pv_node && !in_check && !ss->excluded_move && 
+    depth >= PROBCUT_DEPTH && abs(beta) < MATE_SCORE &&
+    (!tt_hit || tt_depth + 3 < depth || tt_score >= beta + PROBCUT_MARGIN)) {
 
     int16_t probcut_beta = beta + PROBCUT_MARGIN;
     int probcut_depth = depth - PROBCUT_SHALLOW_DEPTH - 1;
