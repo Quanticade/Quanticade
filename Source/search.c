b1f75650127dc35fbda2200dbaa2ecf56e5e041b
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-10-06 09:27:49 +0200

    search.c: Only do NMP on cutnodes
    bench: 3589569

--- Source/search.c
+++ Source/search.c
@@ -806,7 +806,7 @@ static inline int16_t negamax(position_t *pos, thread_t *thread,
     /* search moves with reduced depth to find beta cutoffs
        depth - 1 - R where R is a reduction limit */
     current_score = -negamax(&pos_copy, thread, ss + 1, -beta, -beta + 1,
-                             depth - R, !cutnode, NON_PV);
+                             depth - R, 0, NON_PV);
 
     (ss + 1)->null_move = 0;
 
