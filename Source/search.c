96825273778ad558504db45451ed2bab1a70fabe
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-10-23 09:55:20 +0200

    search.c: Simplify IIR tt_depth condition
    bench: 5291118

--- Source/search.c
+++ Source/search.c
@@ -934,7 +934,7 @@ static inline int16_t negamax(position_t *pos, thread_t *thread,
 
   // Internal Iterative Reductions
   if (!all_node && !ss->excluded_move && depth >= IIR_DEPTH &&
-      (!tt_move || tt_depth < depth - IIR_DEPTH_REDUCTION || tt_flag == HASH_FLAG_UPPER_BOUND)) {
+      (!tt_move || tt_flag == HASH_FLAG_UPPER_BOUND)) {
     depth--;
   }
 
