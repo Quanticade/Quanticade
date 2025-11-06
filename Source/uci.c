33465e6f6806c2f2551dd7133a97298991664a85
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2025-11-06 21:03:16 +0100

    uci.c: Disable soft nodes by default
    Have to add the option for it later
    
    bench: 3172427

--- Source/uci.c
+++ Source/uci.c
@@ -563,7 +563,7 @@ static inline void time_control(position_t *pos, thread_t *threads,
   if ((argument = strstr(line, "nodes"))) {
     // parse amount of time allowed to spend to make a move
     limits.node_limit_soft = atoi(argument + 6);
-    limits.node_limit_hard = 100000000;
+    limits.node_limit_hard = atoi(argument + 6);
     limits.depth = MAX_PLY;
     limits.nodes_set = 1;
   }
