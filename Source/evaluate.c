d6c86a1d36294290b678bc3e6d87dcf39ab7bf08
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2026-01-15 23:43:16 +0000

    evaluate.c: BB mat count
    bench: 4091707

--- Source/evaluate.c
+++ Source/evaluate.c
@@ -9,43 +9,24 @@ nnue_t nnue_data;
 extern nnue_settings_t nnue_settings;
 
 int16_t evaluate(thread_t *thread, position_t *pos, accumulator_t *accumulator) {
-  (void)thread;
-  (void)accumulator;
-  int eval = 0;
-  for (uint8_t sq = 0; sq < 64; sq++) {
-    if (pos->side == 0) {
-      switch (pos->mailbox[sq]) {
-        case P: eval += 100; break;
-        case p: eval -= 100; break;
-        case N: eval += 300; break;
-        case n: eval -= 300; break;
-        case B: eval += 330; break;
-        case b: eval -= 330; break;
-        case R: eval += 500; break;
-        case r: eval -= 500; break;
-        case Q: eval += 900; break;
-        case q: eval -= 900; break;
-        default: break;
-      }
-    }
-    else {
-      switch (pos->mailbox[sq]) {
-        case P: eval -= 100; break;
-        case p: eval += 100; break;
-        case N: eval -= 300; break;
-        case n: eval += 300; break;
-        case B: eval -= 330; break;
-        case b: eval += 330; break;
-        case R: eval -= 500; break;
-        case r: eval += 500; break;
-        case Q: eval -= 900; break;
-        case q: eval += 900; break;
-        default: break;
-      }
-    }
-  }
+    (void)thread;
+    (void)accumulator;
+    int eval = 0;
 
+    // White pieces
+    eval += popcount(pos->bitboards[P]) * 100;
+    eval += popcount(pos->bitboards[N]) * 300;
+    eval += popcount(pos->bitboards[B]) * 330;
+    eval += popcount(pos->bitboards[R]) * 500;
+    eval += popcount(pos->bitboards[Q]) * 900;
+
+    // Black pieces
+    eval -= popcount(pos->bitboards[p]) * 100;
+    eval -= popcount(pos->bitboards[n]) * 300;
+    eval -= popcount(pos->bitboards[b]) * 330;
+    eval -= popcount(pos->bitboards[r]) * 500;
+    eval -= popcount(pos->bitboards[q]) * 900;
 
   int16_t final_eval = clamp(eval, -MATE_SCORE + 1, MATE_SCORE - 1);
-  return final_eval;
+  return pos->side == white ? final_eval : -final_eval;
 }
