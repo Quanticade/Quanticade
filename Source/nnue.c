55b6ab3183a618f262a7a45ff5d6adeb8698e38c
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2026-02-27 01:04:24 +0100

    nnue.c: Remove condition for color in update_nnue
    bench: 2923493

--- Source/nnue.c
+++ Source/nnue.c
@@ -839,19 +839,11 @@ void update_nnue(position_t *pos, thread_t *thread, uint8_t mailbox_copy[64],
   uint8_t white_bucket = get_king_bucket(white, white_king_square);
   uint8_t black_bucket = get_king_bucket(black, black_king_square);
   if (need_refresh(mailbox_copy, move)) {
-    if (pos->side == black) {
       refresh_accumulator(thread, pos, &thread->accumulator[pos->ply]);
       accumulator_make_move(&thread->accumulator[pos->ply],
                             &thread->accumulator[pos->ply - 1],
                             white_king_square, black_king_square, white_bucket,
-                            black_bucket, pos->side, move, mailbox_copy, black);
-    } else if (pos->side == white) {
-      refresh_accumulator(thread, pos, &thread->accumulator[pos->ply]);
-      accumulator_make_move(&thread->accumulator[pos->ply],
-                            &thread->accumulator[pos->ply - 1],
-                            white_king_square, black_king_square, white_bucket,
-                            black_bucket, pos->side, move, mailbox_copy, white);
-    }
+                            black_bucket, pos->side, move, mailbox_copy, pos->side);
   } else {
     accumulator_make_move(&thread->accumulator[pos->ply],
                           &thread->accumulator[pos->ply - 1], white_king_square,
