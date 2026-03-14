1cca101059b759e52c76299206a57947a0a2dc29
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2026-03-14 23:24:56 +0100

    datagen: book datagen
    bench: 3241901

--- Source/datagen.c
+++ Source/datagen.c
@@ -7,6 +7,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+int random_fen_from_file(const char *filename, char *out_fen, size_t max_len) {
+    FILE *f = fopen(filename, "r");
+    if (!f) return 0;
+
+    char buffer[256];
+    uint64_t count = 0;
+
+    while (fgets(buffer, sizeof(buffer), f)) {
+        count++;
+
+        if (rand() % count == 0) {
+            strncpy(out_fen, buffer, max_len - 1);
+            out_fen[max_len - 1] = '\0';
+
+            // remove newline
+            char *nl = strchr(out_fen, '\n');
+            if (nl) *nl = '\0';
+        }
+    }
+
+    fclose(f);
+
+    return count > 0;
+}
+
 uint8_t play_rand_moves(position_t *pos, thread_t * thread, uint8_t rand_moves) {
     moves pseudo_moves[1];
     moves legal_moves[1];
@@ -46,16 +71,33 @@ uint8_t play_rand_moves(position_t *pos, thread_t * thread, uint8_t rand_moves) 
     return play_rand_moves(&pos_copy, thread, rand_moves - 1);
 }
 
-void genfens(position_t *pos, thread_t * thread, uint64_t seed, uint16_t n_of_fens) {
+void genfens(position_t *pos, thread_t *thread, uint64_t seed,
+             uint16_t n_of_fens, const char *bookfile) {
+
     srand(seed);
 
     int generated_fens = 0;
+
     while (generated_fens < n_of_fens) {
-        position_t pos_copy = *pos;
-        // 6-9 random moves from startpos
-        int random_moves = 6 + rand() % 4;
-        if (play_rand_moves(&pos_copy, thread, random_moves)) {
-            generated_fens++;
+
+        if (bookfile != NULL && strcmp(bookfile, "None") != 0) {
+            char fen[256];
+
+            if (!random_fen_from_file(bookfile, fen, sizeof(fen)))
+                return;
+
+            char input[512];
+            sprintf(input, "position fen %s", fen);
+            parse_position(pos, thread, input);
+            init_accumulator(pos, &thread->accumulator[pos->ply]);
+            init_finny_tables(thread, pos);
         }
+
+        position_t pos_copy = *pos;
+
+        int random_moves = 6 + rand() % 4;
+
+        if (play_rand_moves(&pos_copy, thread, random_moves))
+            generated_fens++;
     }
 }
