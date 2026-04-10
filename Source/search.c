cfbacaa40338baef705963c61c15a6303ee12fc0
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2026-04-10 22:06:26 +0200

    search.c: Do LMR later if its a root node (#342)
    bench: 3091595

--- Source/search.c
+++ Source/search.c
@@ -1169,7 +1169,7 @@ static inline int16_t negamax(thread_t *thread, searchstack_t *ss,
     int new_depth = depth + extensions - 1;
 
     // LMR
-    if (depth >= 2 && moves_seen > 1) {
+    if (depth >= 2 && moves_seen > 1 + root_node) {
       int R = lmr[quiet][depth][MIN(255, moves_seen)] * 1024;
       R += !pv_node * LMR_PV_NODE;
       R -= ss->history_score * (quiet ? LMR_HISTORY_QUIET : LMR_HISTORY_NOISY) /
