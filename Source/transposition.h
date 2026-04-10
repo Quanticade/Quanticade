c97bfa0d640aa7ec972abbf1d919577509074898
Martin Botka <16738302+Haxk20@users.noreply.github.com>
2026-04-10 19:17:38 +0200

    Source: implement huge pages on linux (#341)
    bench: 3066465

--- Source/transposition.h
+++ Source/transposition.h
@@ -1,6 +1,5 @@
 #ifndef TRANSPOSITION_H
 #define TRANSPOSITION_H
-
 #include "structs.h"
 #include <sched.h>
 
@@ -8,7 +7,6 @@ typedef struct tt {
   tt_bucket_t *hash_entry;
   size_t num_of_entries;
 } tt_t;
-
 extern tt_t tt;
 
 // transposition table hash flags
@@ -18,14 +16,16 @@ extern tt_t tt;
 #define HASH_FLAG_UPPER_BOUND 3
 
 void clear_hash_table(void);
+void free_hash_table(void);
 void prefetch_hash_entry(uint64_t hash_key);
 uint8_t can_use_score(int alpha, int beta, int tt_score, uint8_t flag);
 int16_t score_from_tt(const uint8_t ply, int16_t score);
 tt_entry_t* read_hash_entry(position_t *pos, uint8_t *tt_hit);
 void write_hash_entry(tt_entry_t *tt_entry, position_t *pos, const uint8_t ply, int16_t score,
-                      int16_t static_eval, uint8_t depth, uint16_t move,
-                      uint8_t hash_flag, uint8_t tt_pv);
+int16_t static_eval, uint8_t depth, uint16_t move,
+uint8_t hash_flag, uint8_t tt_pv);
 void init_hash_table(uint64_t mb);
 uint64_t generate_hash_key(position_t *pos);
 int hash_full(void);
+
 #endif
