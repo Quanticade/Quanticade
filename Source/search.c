30982af71dd2f28b420a9e0916864e8c794e2708
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2026-05-01 21:46:13 +0200

    search.c: Adjust LMR for ttpv depth
    bench: 3605743

--- Source/search.c
+++ Source/search.c
@@ -1240,6 +1240,7 @@ static inline int16_t negamax(thread_t *thread, searchstack_t *ss,
       R -= (tt_depth >= depth) * LMR_TT_DEPTH;
       R -= ss->tt_pv * LMR_TT_PV;
       R += (ss->tt_pv && tt_hit && tt_score <= alpha) * LMR_TT_SCORE;
+      R -= (ss->tt_pv && tt_hit && tt_depth >= depth) * 1024;
       R -= (ss->tt_pv && cutnode) * LMR_TT_PV_CUTNODE;
       R -= stm_in_check(next_pos) * LMR_IN_CHECK; // check on the new position
       R += (ss->cutoff_cnt > 3) * LMR_CUTOFF_CNT;
