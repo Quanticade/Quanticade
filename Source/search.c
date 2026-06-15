1a11a5e923818bc3214ef9a120075997126d4226
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2026-06-15 23:04:28 +0200

    search.c: Use factorized quadratic formula for quiet SEE
    bench: 2955943

--- Source/search.c
+++ Source/search.c
@@ -1213,7 +1213,7 @@ static inline int16_t negamax(thread_t *thread, searchstack_t *ss,
       int see_treshold;
       if (!get_move_capture(move)) {
         see_treshold =
-            -SEE_QUIET * depth - ss->history_score / SEE_QUIET_HISTORY_DIVISOR;
+            (-256 * depth * depth - 7936 * depth - 5632) / 256 - ss->history_score / SEE_QUIET_HISTORY_DIVISOR;
       } else {
         see_treshold = -SEE_CAPTURE * depth * depth -
                        ss->history_score / SEE_NOISY_HISTORY_DIVISOR;
