9df1a9354ec1724de4a2f1b4d0a671ad6713c507
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2026-06-21 15:29:18 +0200

    search.c: TT cutoff only when tt score below alpha or cutnode
    bench: 3173879

--- Source/search.c
+++ Source/search.c
@@ -793,6 +793,7 @@ static inline int16_t negamax(thread_t *thread, searchstack_t *ss,
   // If we arent in excluded move or PV node and we hit requirements for cutoff
   // we can return early from search
   if (!ss->excluded_move && !pv_node && tt_depth >= depth &&
+      (tt_score <= alpha || cutnode) &&
       can_use_score(alpha, beta, tt_score, tt_flag)) {
     if (tt_move != 0 &&
         !(is_move_promotion(tt_move) || get_move_capture(tt_move)) &&
