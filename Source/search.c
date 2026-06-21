72ad3c2d4ab35e7f55c1ea1a3817ac7c7fd2e90c
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2026-06-21 15:22:06 +0200

    search.c: Switch probcut to TT eval
    bench: 2941033

--- Source/search.c
+++ Source/search.c
@@ -992,7 +992,7 @@ static inline int16_t negamax(thread_t *thread, searchstack_t *ss,
     while ((move = select_next(&probcut_picker)) != 0) {
 
       // Skip moves that don't pass SEE threshold
-      if (!SEE(pos, move, probcut_beta - ss->static_eval)) {
+      if (!SEE(pos, move, probcut_beta - ss->eval)) {
         continue;
       }
 
