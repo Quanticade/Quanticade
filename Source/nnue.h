ec9c3b05e30dd407361e40763746d06f0c7e501c
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2026-06-29 06:33:08 +0200

    faster ti updates
    bench: 3497131

--- Source/nnue.h
+++ Source/nnue.h
@@ -8,7 +8,7 @@
 extern nnue_settings_t nnue_settings;
 
 typedef struct nnue {
-  _Alignas(64) int8_t feature_threats[THREAT_FEATURES][L1_SIZE];
+  _Alignas(64) int8_t  feature_threats[THREAT_FEATURES][L1_SIZE];
   _Alignas(64) int16_t feature_weights[KING_BUCKETS][PSQT_FEATURES][L1_SIZE];
   _Alignas(64) int16_t feature_bias[L1_SIZE];
   _Alignas(64) int8_t  l1_weights[OUTPUT_BUCKETS][L1_SIZE * L2_SIZE];
